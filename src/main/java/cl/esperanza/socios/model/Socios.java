a8b80b7cfde44ff0598b851d40e5582fef6058f4
TheGhouzt <elrandom989@gmail.com>
2026-05-18 12:12:43 -0400

    0.1

---
+++ src/main/java/cl/esperanza/socios/model/Socios.java
@@ -0,0 +1,42 @@
+package cl.esperanza.socios.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "socios")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Socios {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(unique = true, length = 13, nullable = true) 
+    private String run;
+
+    @Column(unique = true, length = 13, nullable = true) 
+    private int telefono;
+
+    @Column(nullable = true)
+    private String nombre;
+
+    @Column(nullable = true)
+    private String apellido;
+
+    @Column(nullable = true)
+    private String direccion;
+
+
+    @Column(nullable = false)
+    private String correo;
+}
---
+++ src/test/java/cl/esperanza/socios/SociosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.esperanza.socios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SociosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
